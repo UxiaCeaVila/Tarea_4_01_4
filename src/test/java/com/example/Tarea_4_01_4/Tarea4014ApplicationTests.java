88bd430a3fefe46ca9dfefb3d43cbed8793395f5
UxiaCeaVila <105040856+UxiaCeaVila@users.noreply.github.com>
2023-01-17 09:14:17 +0100

    Initial commit

---
+++ src/test/java/com/example/Tarea_4_01_4/Tarea4014ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tarea_4_01_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea4014ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
